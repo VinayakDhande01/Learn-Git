93cf518cd7655d624d51e51f17b5fefb1b2eadc7
Vinayak Dhande <vinayak735071@gmail.com>
2025-08-04 14:09:24 +0530

    sum updated

--- Sum.java
+++ Sum.java
@@ -1,8 +1,9 @@
 public class Sum {
-    public static void main(String[] args){
-         int a=10;
-         int b=20;
-         System.out.println("Sum of a+b="+ (a+b));
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c=30;   
+         System.out.println("Sum of a+b="+ (a+b+c));
 
     }
 
